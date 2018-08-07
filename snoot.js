3fe283a08154f7ab00b47fcde8deb921daabea0c
Tiffany Altagracia <taltagracia844@west-mec.org>
2018-08-07 15:34:04 -0700

    set up the list of days based the selected Month and set up doc frags

--- snoot.js
+++ snoot.js
@@ -5,10 +5,51 @@
 // Date: 8.6.18
 "use strict";
 
+var twentyNine = document.createDocumentFragment();
+var thirty = document.createDocumentFragment();
+var thirtyOne = document.createDocumentFragment();
+
 // Function to remove select list defaults 
 function removeSelectDefaults() {
     var emptyBoxes = document.getElementsByTagName("select");
-    alert("select list:" + emptyBoxes.length);
+    for (var i = 0; i < emptyBoxes.length; i++) {
+        emptyBoxes[i].selectedIndex = -1;
+    }
+}
+
+// Function to set up document fragments for days of the month
+function setUpDays() {
+    var dates = document.getElementsByTagName("delivDy").getElementsByTagName("option");
+    twentyNine.appendChild(dates[28].cloneNode(true));
+    thirty.appendChild(dates[28].cloneNode(true));
+    thirty.appendChild(dates[29].cloneNode(true));
+    thirtyOne.appendChild(dates[28].cloneNode(true));
+    thirtyOne.appendChild(dates[29].cloneNode(true));
+    thirtyOne.appendChild(dates[30].cloneNode(true));
+
+}
+
+
+// Functions thats sets up page on load events 
+function setUpPage() {
+    removeSelectDefaults();
+setUpDays();
+updateDays();
+}
+
+// Function to set up the list of days based the selected Month
+function updateDays() {
+    var deliveryDay = document.getElementById("delivDy");
+    var dates = deliveryDay.getElementsByTagName("option");
+    var deliveryMonth = document.getElementById("delivMo");
+    var deliveryYear = document.getElementById("delivYr");
+    var selectedMonth = deliveryMonth.option[deliveryMonth.selectedIndex].value;
+    while (dates[28]) {
+        deliveryDay.removeChild(dates[28]);
+    }
+    if(deliveryYear.selectedIndex === -1){
+deliveryDay.selectedIndex = 0;
+    }
 }
 
 // Page loads event handlers
