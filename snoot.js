11848833bd0f3a47e7615560e9ce5e5232054740
Tiffany Altagracia <taltagracia844@west-mec.org>
2018-08-13 15:27:17 -0700

    validated fieldset and added error messages and styles

--- snoot.js
+++ snoot.js
@@ -3,14 +3,15 @@
 
 // Author: Tiffany Altagracia
 // Date: 8.6.18
-"use strict";
 
+"use strict";
 
 var twentyNine = document.createDocumentFragment();
 var thirty = document.createDocumentFragment();
 var thirtyOne = document.createDocumentFragment();
+var formValidity = true;
 
-// Function to remove select list defaults 
+// function to remove select list defaults
 function removeSelectDefaults() {
     var emptyBoxes = document.getElementsByTagName("select");
     for (var i = 0; i < emptyBoxes.length; i++) {
@@ -18,8 +19,7 @@ function removeSelectDefaults() {
     }
 }
 
-
-// Function to set up document fragments for days of the month
+// function to set up document fragments for days of the month
 function setUpDays() {
     var dates = document.getElementById("delivDy").getElementsByTagName("option");
     twentyNine.appendChild(dates[28].cloneNode(true));
@@ -28,120 +28,191 @@ function setUpDays() {
     thirtyOne.appendChild(dates[28].cloneNode(true));
     thirtyOne.appendChild(dates[29].cloneNode(true));
     thirtyOne.appendChild(dates[30].cloneNode(true));
-
 }
-// Function to inspect custom check box on message 
+
+// function to inspect custom check box on message change
 function autoCheckCustom() {
     var messageBox = document.getElementById("customText");
-    //Textarea has a message, check the box 
+    // textarea has message, check the box
     if (messageBox.value !== "" && messageBox.value !== messageBox.placeholder) {
         document.getElementById("custom").checked = "checked";
     }
-    // Textarea has no message, un-check the box 
+    // texture has no message, uncheck the box
     else {
         document.getElementById("custom").checked = "";
     }
 }
 
-// function to copy billing to delivery address 
+// function to copy billing address to delivery address
 function copyBillingAddress() {
     var billingInputElements = document.querySelectorAll("#billingAddress input");
     var deliveryInputElements = document.querySelectorAll("#deliveryAddress input");
-    //duplicate address - checkbox is checked - copy
+    // duplicate address - checkbox is checked - copy
     if (document.getElementById("sameAddr").checked) {
         for (var i = 0; i < billingInputElements.length; i++) {
-            deliveryInputElements[i + 1].value =
-                billingInputElements[i].value;
+            deliveryInputElements[i + 1].value = billingInputElements[i].value;
         }
-        document.querySelector("#deliveryAddress select").value = document.querySelector("#billingAddress select").value
+        document.querySelector("#deliveryAddress select").value = document.querySelector("#billingAddress select").value;
+
     }
-    // duplicate address - checkboxnot checked - erase
-    else{
+    // duplicate address - checkbox is not checked - erase
+    else {
         for (var i = 0; i < billingInputElements.length; i++) {
             deliveryInputElements[i + 1].value = "";
-             
         }
         document.querySelector("#deliveryAddress select").selectedIndex = -1;
-  
     }
 }
 
-// Functions thats sets up page on load events 
-function setUpPage() {
-    removeSelectDefaults();
-    setUpDays();
+// Function to validate address - billing and delivery
+function validateAddress(fieldsetId) {
+    var inputElements = document.querySelectorAll("#" + fieldsetId + " input");
+    var errorDiv = document.querySelectorAll("#" + fieldsetId + " .errorMessage") [0];
+    var fieldsetValidity = true;
+    var elementCount = inputElements.length;
+    var currentElement;
+    try {
 
-    createEventListeners();
+        // loop through input fields looking for blank
+        for (var i = 0; i < elementCount; i++) {
+            currentElement = inputElements[i];
+            // blanks 
+            if (currentElement.value === "") {
+              
+                currentElement.style.background = "rgb(255,233,233)";
+                fieldsetValidity = false;
+            } else {
+                currentElement.style.background = "white";
+            }
+        }
+        // validate select list field 
+        currentElement = document.querySelectorAll('#' + fieldsetId + "select")[0];
+        if(currentElement.selectedIndex === -1){
+            currentElement.style.border = " 1px solid red";
+            fieldsetValidity = false;
+        }
+        else{
+            currentElement.style.border = "";
 
-}
+        }
 
-// Function to create our event listeners
-function createEventListeners() {
-    var deliveryMonth = document.getElementById("delivMo");
-
-    if (deliveryMonth.addEventListener) {
-        deliveryMonth.addEventListener("change", updateDays, false);
-    } else if (deliveryMonth.attachEvent) {
-        deliveryMonth.attachEvent("onchange", updateDays);
+        if (fieldsetValidity === false) {
+            if (fieldsetId === "billingAddress") {
+                throw "Please complete all Billing Address information."
+            } else {
+                throw "Please complete all Delivery Address information."
+            }
+        }
+        else{
+            errorDiv.style.display = "none";
+            errorDiv.innerHTML = "";
+        }
+        } catch (msg) {
+            errorDiv.style.display = "block";
+            errorDiv.innerHTML = msg;
+            formValidity = false;
+        }
     }
-    var deliveryYear = document.getElementById("delivYr");
 
-    if (deliveryYear.addEventListener) {
-        deliveryYear.addEventListener("change", updateDays, false);
-    } else if (deliveryYear.attachEvent) {
-        deliveryYear.attachEvent("onchange", updateDays);
+    // function to validate entire form
+    function validateForm(evt) {
+        //    alert("submit event");
+        if (evt.preventDefault) {
+            evt.preventDefault();
+        } else {
+            evt.returnValue = false;
+        }
+        formValidity = true;
+
+        validateAddress("billingAddress");
+        validateAddress("deliveryAddress");
+
+        if (formValidity === true) { // form is valid
+            document.getElementById("errorText").innerHTML = "";
+            document.getElementById("errorText").style.display = "none";
+            document.getElementsByTagName("form")[0].submit();
+        } else {
+            document.getElementById("errorText").innerHTML = "Please fix the indicated problems and then resubmit your order.";
+            document.getElementById("errorText").style.display = "block";
+        }
     }
-    var messageBox = document.getElementById("customText");
 
-    if (messageBox.addEventListener) {
-        messageBox.addEventListener("change", autoCheckCustom, false);
-
-    } else if (messageBox.attachEvent) {
-        messageBox.attachEvent("onchange", autoCheckCustom);
+    // function that sets up page on load event
+    function setUpPage() {
+        removeSelectDefaults();
+        setUpDays();
+        createEventListeners();
     }
-    var same = document.getElementById("sameAddr");
-    if (same.addEventListener) {
-        same.addEventListener("change", copyBillingAddress, false);
 
-    } else if (same.attachEvent) {
-        same.attachEvent("onchange", copyBillingAddress);
+    // function to create our event listeners
+    function createEventListeners() {
+        var deliveryMonth = document.getElementById("delivMo");
+        if (window.addEventListener) {
+            deliveryMonth.addEventListener("change", updateDays, false);
+        } else if (deliveryMonth.attachEvent) {
+            deliveryMonth.attachEvent("onchange", updateDays);
+        }
+        var deliveryYear = document.getElementById("delivYr");
+        if (deliveryYear.addEventListener) {
+            deliveryYear.addEventListener("change", updateDays, false);
+        } else if (deliveryYear.attachEvent) {
+            deliveryYear.attachEvent("onchange", updateDays);
+        }
+        var messageBox = document.getElementById("customText");
+        if (messageBox.addEventListener) {
+            messageBox.addEventListener("change", autoCheckCustom, false);
+        } else if (messageBox.attachEvent) {
+            messageBox.attachEvent("onchange", autoCheckCustom);
+        }
+        var same = document.getElementById("sameAddr");
+        if (same.addEventListener) {
+            same.addEventListener("change", copyBillingAddress, false);
+        } else if (same.attachEvent) {
+            same.attachEvent("onchange", copyBillingAddress);
+        }
+        var form = document.getElementsByTagName("form")[0];
+        if (form.addEventListener) {
+            form.addEventListener("submit", validateForm, false);
+        } else if (form.attachEvent) {
+            form.attachEvent("onsubmit", validateForm);
+        }
     }
-}
 
+    // function that sets up list of days based on selected month
+    function updateDays() {
+        var deliveryDay = document.getElementById("delivDy");
+        var dates = deliveryDay.getElementsByTagName("option");
+        var deliveryMonth = document.getElementById("delivMo");
+        var deliveryYear = document.getElementById("delivYr");
+        var selectedMonth = deliveryMonth.options[deliveryMonth.selectedIndex].value;
+        while (dates[28]) {
+            deliveryDay.removeChild(dates[28]);
+        }
+        if (deliveryYear.selectedIndex === -1) {
+            deliveryDay.selectedIndex = 0;
+        }
 
+        // If feb and 2020 twentyNine
+        if (selectedMonth === "2" && deliveryYear.options[deliveryYear.selectedIndex].value === "2020") {
+            deliveryDay.appendChild(twentyNine.cloneNode(true));
+        }
 
-// Function to set up the list of days based the selected Month
-function updateDays() {
-    var deliveryDay = document.getElementById("delivDy");
-    var dates = deliveryDay.getElementsByTagName("option");
-    var deliveryMonth = document.getElementById("delivMo");
-    var deliveryYear = document.getElementById("delivYr");
-    var selectedMonth = deliveryMonth.option[deliveryMonth.selectedIndex].value;
-    while (dates[28]) {
-        deliveryDay.removeChild(dates[28]);
+        // else if 30 day month thirty
+        else if (selectedMonth === "4" || selectedMonth === "6" || selectedMonth === "9" || selectedMonth === "11") {
+            deliveryDay.appendChild(thirty.cloneNode(true));
+        }
+
+        // else if 31 day month thirtyOne
+        else if (selectedMonth === "1" || selectedMonth === "3" || selectedMonth === "5" || selectedMonth === "7" || selectedMonth === "8" || selectedMonth === "10" || selectedMonth === "12") {
+            deliveryDay.appendChild(thirtyOne.cloneNode(true));
+        }
     }
-    if (deliveryYear.selectedIndex === -1) {
-        deliveryDay.selectedIndex = 0;
-    }
-    // If it is feb and 2020 twentyNine
-    if (selectedMonth === '2' && deliveryYear.option[deliveryYear].value === "2020") {
-        deliveryDay.appendChild(twentyNine.cloneNode(true));
-    }
-    // else if 30 day month thirty
-    else if (selectedMonth === "4" || selectedMonth === "6" || selectedMonth === "9" || selectedMonth === "11") {
-        deliveryDay.appendChild(thirty.cloneNode(true));
-    }
-    //else if 31 day month thirtyOne
-    else if (selectedMonth === "1" || selectedMonth === "3" || selectedMonth === "5" || selectedMonth === "7" || selectedMonth === "8" || selectedMonth === "12") {
-        deliveryDay.appendChild(thirtyOne.cloneNode(true));
-    }
-}
 
+    // else 
 
-// Page loads event handlers
-
-if (window.addEventListener) {
-    window.addEventListener("load", setUpPage, false);
-} else if (window.attachEvent) {
-    window.attachEvent("onload", setUpPage);
-}
+    // page load event handlers
+    if (window.addEventListener) {
+        window.addEventListener("load", setUpPage, false);
+    } else if (window.attachEvent) {
+        window.attachEvent("onload", setUpPage);
+    }
